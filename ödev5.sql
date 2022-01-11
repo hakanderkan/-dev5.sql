f29e0e979653ad6be8c8fbc5d14a810ff61b0a1e
hakanderkan <90130889+hakanderkan@users.noreply.github.com>
2022-01-12 00:29:32 +0300

    ödev5.sql

---
+++ ödev5.sql
@@ -0,0 +1,15 @@
+ödev1- select * from film 
+where title ILIKE '%n' 
+order by length desc
+limit 5;
+
+ödev2- select * from film 
+where title ILIKE '%n'
+Order by length desc 
+ offset 5
+limit 5;
+
+ödev3-) select * from customer
+where store_id = 1
+order by last_name desc 
+limit 4;
